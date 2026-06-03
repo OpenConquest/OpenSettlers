25d267e8feddd4437c870cd3b02f739f13d4f4a1
julien.glin <julien.glin@epita.fr>
2026-06-03 11:05:39 +0200

    feat: add BuildingName enum and static buildingCosts map for resource management

--- backend/src/main/java/fr/opensettlers/model/Building.java
+++ backend/src/main/java/fr/opensettlers/model/Building.java
@@ -31,4 +31,33 @@ public abstract class Building {
      * Flag currently attached to the building, if any. A building can have at most one flag attached at a time.
      */
     private final Flag attachedFlag;
+
+    /**
+     * A static map that defines the resource costs for constructing each type of building. The key is the BuildingName enum value, and the value is another map that specifies the required resources and their quantities for that building.
+     */
+    public static final Map<BuildingName, Map<ResourceType, Integer>> buildingCosts = Map.ofEntries(
+            Map.entry(BuildingName.WOODCUTTER, Map.of(ResourceType.PLANK, 2)),
+            Map.entry(BuildingName.FORESTER, Map.of(ResourceType.PLANK, 2)),
+            Map.entry(BuildingName.QUARRY, Map.of(ResourceType.PLANK, 2)),
+            Map.entry(BuildingName.MINE, Map.of(ResourceType.PLANK, 4)),
+            Map.entry(BuildingName.FISHING_HUT, Map.of(ResourceType.PLANK, 2)),
+            Map.entry(BuildingName.WATER_WELL, Map.of(ResourceType.PLANK, 2)),
+            Map.entry(BuildingName.BARRACKS, Map.of(ResourceType.PLANK, 2 )),
+            Map.entry(BuildingName.GUARD_HOUSE, Map.of(ResourceType.PLANK, 2, ResourceType.STONE, 3)),
+
+            Map.entry(BuildingName.BREWERY, Map.of(ResourceType.PLANK, 2, ResourceType.STONE, 2)),
+            Map.entry(BuildingName.FOUNDRY, Map.of(ResourceType.PLANK, 2, ResourceType.STONE, 2)),
+            Map.entry(BuildingName.MILL, Map.of(ResourceType.PLANK, 2, ResourceType.STONE, 2)),
+            Map.entry(BuildingName.ARMORY, Map.of(ResourceType.PLANK, 2, ResourceType.STONE, 2)),
+            Map.entry(BuildingName.BAKERY, Map.of(ResourceType.PLANK, 2, ResourceType.STONE, 2)),
+            Map.entry(BuildingName.SAWMILL, Map.of(ResourceType.PLANK, 2, ResourceType.STONE, 2)),
+            Map.entry(BuildingName.WATCH_TOWER, Map.of(ResourceType.PLANK, 3, ResourceType.STONE, 5)),
+            Map.entry(BuildingName.WAREHOUSE, Map.of(
+                    ResourceType.PLANK, 4,
+                    ResourceType.STONE, 3
+            )),
+
+            Map.entry(BuildingName.FARM, Map.of(ResourceType.PLANK, 3, ResourceType.STONE,3)),
+            Map.entry(BuildingName.CASTLE, Map.of(ResourceType.PLANK, 4, ResourceType.STONE, 7))
+    );
 }
